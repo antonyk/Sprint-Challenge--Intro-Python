609fe20019316f559d7de038d8f57b6db307c7b0
Anthony K <antonyk@users.noreply.github.com>
2020-06-05 14:24:34 -0400

    print formatting

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -111,14 +111,17 @@ def cityreader_stretch(lat1, lon1, lat2, lon2, cities=[]):
 
 
 while True:
+  print('\n')
   pt1 = [item.strip() for item in input("Enter lat1, lon1:").split(',')]
   pt2 = [item.strip() for item in input("Enter lat2, lon2:").split(',')]
 
+  print('\n')
   try:
     result = cityreader_stretch(float(pt1[0]), float(pt1[1]), float(pt2[0]), float(pt2[1]), cities)
+    print('Cities within square:')
+    print('---------------------')
     for item in result:
       print(f"{item.name}: ({item.lat},{item.lon})")
   except:
     print("Incorrect input! Try again")
 
-  print('\n')
