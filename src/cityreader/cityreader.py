cc406708cdb8bd5b7dcf38c28990965804f43fd4
Anthony K <antonyk@users.noreply.github.com>
2020-09-04 12:34:17 -0400

    refactor and format cityreader

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -2,20 +2,22 @@
 # fields for name, lat and lon (representing latitude and longitude).
 import csv
 
-class City:
-  def __init__(self, name, lat, lon):
-    self.name = name
-    self.lat = lat
-    self.lon = lon
 
-  def __str__(self):
-    # return f"{self.name} {self.lat} {self.lon}"
-    return f"({self.name}, {self.lat}, {self.lon})"
+class City:
+    def __init__(self, name, lat, lon):
+        self.name = name
+        self.lat = lat
+        self.lon = lon
+
+    def __str__(self):
+        # return f"{self.name} {self.lat} {self.lon}"
+        return f"({self.name}, {self.lat}, {self.lon})"
+
 
 # We have a collection of US cities with population over 750,000 stored in the
 # file "cities.csv". (CSV stands for "comma-separated values".)
 #
-# In the body of the `cityreader` function, use Python's built-in "csv" module 
+# In the body of the `cityreader` function, use Python's built-in "csv" module
 # to read this file so that each record is imported into a City instance. Then
 # return the list with all the City instances from the function.
 # Google "python 3 csv" for references and use your Google-fu for other examples.
@@ -26,14 +28,17 @@ class City:
 # should not be loaded into a City object.
 cities = []
 
+
 def cityreader(cities=[]):
-  # TODO Implement the functionality to read from the 'cities.csv' file
-  # For each city record, create a new City instance and add it to the 
-  # `cities` list
-  with open('cities.csv', newline='') as csvfile:
-    csvreader = csv.reader(csvfile, delimiter=',')
-    cities.extend([City(row[0], float(row[3]), float(row[4])) for row in csvreader if row[0] != 'city'])
-  return cities
+    # TODO Implement the functionality to read from the 'cities.csv' file
+    # For each city record, create a new City instance and add it to the
+    # `cities` list
+    with open('cities.csv', newline='') as csvfile:
+        csvreader = csv.reader(csvfile, delimiter=',')
+        cities.extend([City(row[0], float(row[3]), float(row[4]))
+                       for row in csvreader if row[0] != 'city'])
+    return cities
+
 
 cityreader(cities)
 
@@ -44,10 +49,10 @@ for c in cities:
 # STRETCH GOAL!
 #
 # Allow the user to input two points, each specified by latitude and longitude.
-# These points form the corners of a lat/lon square. Pass these latitude and 
+# These points form the corners of a lat/lon square. Pass these latitude and
 # longitude values as parameters to the `cityreader_stretch` function, along
 # with the `cities` list that holds all the City instances from the `cityreader`
-# function. This function should output all the cities that fall within the 
+# function. This function should output all the cities that fall within the
 # coordinate square.
 #
 # Be aware that the user could specify either a lower-left/upper-right pair of
@@ -74,54 +79,59 @@ for c in cities:
 
 
 class Coord:
-  def __init__(self, x, y):
-    self.x = x
-    self.y = y
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
 
-class Square:
-  def __init__(self, a, b):
-    self.A = a
-    self.B = b
 
-  def contains(self, pt):
-    bot = min(self.A.x, self.B.x)
-    top = max(self.A.x, self.B.x)
-    left = min(self.A.y, self.B.y)
-    right = max(self.A.y, self.B.y)
-    
-    # bot = self.A.x if self.A.x > self.B.x else self.B.x
-    # top = self.A.x if self.A.x > self.B.x else self.B.x
+class Rect:
+    def __init__(self, a, b):
+        self.A = a
+        self.B = b
 
-    if (pt.x >= bot and pt.x <= top) and (pt.y >= left and pt.y <= right):
-      return True
-    else:
-      return False
+    def contains(self, pt):
+        bot = min(self.A.x, self.B.x)
+        top = max(self.A.x, self.B.x)
+        left = min(self.A.y, self.B.y)
+        right = max(self.A.y, self.B.y)
+
+        # bot = self.A.x if self.A.x > self.B.x else self.B.x
+        # top = self.A.x if self.A.x > self.B.x else self.B.x
+
+        if (pt.x >= bot and pt.x <= top) and (pt.y >= left and pt.y <= right):
+            return True
+        else:
+            return False
+
 
 def cityreader_stretch(lat1, lon1, lat2, lon2, cities=[]):
-  # within will hold the cities that fall within the specified region
-  within = []
+    # within will hold the cities that fall within the specified region
+    within = []
 
-  # TODO Ensure that the lat and lon valuse are all floats
-  # Go through each city and check to see if it falls within 
-  # the specified coordinates.
-  square = Square(Coord(lat1, lon1), Coord(lat2, lon2))
-  within = [item for item in cities if square.contains(Coord(item.lat, item.lon))]
+    # TODO Ensure that the lat and lon valuse are all floats
+    # Go through each city and check to see if it falls within
+    # the specified coordinates.
+    rect = Rect(Coord(lat1, lon1), Coord(lat2, lon2))
+    within = [item for item in cities if rect.contains(
+        Coord(item.lat, item.lon))]
 
-  return within
+    return within
 
 
 while True:
-  print('\n')
-  pt1 = [item.strip() for item in input("Enter lat1, lon1:").split(',')]
-  pt2 = [item.strip() for item in input("Enter lat2, lon2:").split(',')]
+    print('\n')
+    pt1 = [item.strip() for item in input(
+        "Enter coordinates (lat, lon) of rectangle's first corner: ").split(',')]
+    pt2 = [item.strip() for item in input(
+        "Enter coordinates (lat, lon) of rectangle's second corner: ").split(',')]
 
-  print('\n')
-  try:
-    result = cityreader_stretch(float(pt1[0]), float(pt1[1]), float(pt2[0]), float(pt2[1]), cities)
-    print('Cities within square:')
-    print('---------------------')
-    for item in result:
-      print(f"{item.name}: ({item.lat},{item.lon})")
-  except:
-    print("Incorrect input! Try again")
-
+    print('\n')
+    try:
+        result = cityreader_stretch(float(pt1[0]), float(
+            pt1[1]), float(pt2[0]), float(pt2[1]), cities)
+        print('Cities within square:')
+        print('---------------------')
+        for item in result:
+            print(f"{item.name}: ({item.lat},{item.lon})")
+    except:
+        print("Incorrect input! Try again")
